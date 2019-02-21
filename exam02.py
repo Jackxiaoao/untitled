ba614ad6daa9b4d82fb2bc7986e2505e904b7629
acer <1434657296@qq.com>
2019-02-21 16:33:10 +0800

    22

---
+++ exam02.py
@@ -0,0 +1,6 @@
+# var1 = input("请输入第一个数:")
+# var2 = input("请输入第二个数:")
+# print("两个数拼接:",var1 + var2)
+
+var1 = float(input("请输入一个数:"))
+print(type(int(var1)))
---
+++ exam03.py
@@ -0,0 +1,16 @@
+# var1 = input("请输入第一个生日:")
+# var2 = input("请输入第二个生日:")
+# if int(var1 > var2) :
+#     print("第一个人年龄大")
+# else :
+#     print("第二个人年龄大")
+
+#
+# import random
+# var1 = random.randrange(1,10)
+# var2 = random.randrange(10)
+# if var1 == var2 :
+#     print("在一个序列",var1,var2)
+# else :
+#     print("不在一个序列")
+
